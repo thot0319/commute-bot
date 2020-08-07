515831b0d8ac09445ec8f038674ec104a74b2f0e
DISCORDBBBB <60780527+BSGreatuser@users.noreply.github.com>
2020-08-07 09:29:32 +0900

    Update commute.py

--- commute.py
+++ commute.py
@@ -1,7 +1,4 @@
-# https://youtube.com/c/봉순bs
-
 import discord
-from discord.ext import commands
 
 client = discord.Client()
 
@@ -12,20 +9,20 @@ async def on_ready():
     game = discord.Game('★표시될 게임이름★')
     await client.change_presence(status=discord.Status.online, activity=game)
 
-# PERMISSION ERROR
 
 @client.event
-@commands.has_permissions(manage_messages=True)
 async def on_message(message):
     if message.content.startswith("/출근"):
         try:
-            author = message.guild.get_member(int(message.author.id))
-            embed = discord.Embed(color=0x80E12A)
-            channel = ★전송될 채널 아이디★
-            embed.set_author(name=author, icon_url=message.author.avatar_url)
-            embed.add_field(name='관리자 출퇴근 알림', value='관리자가 출근하였습니다.')
-            embed.set_image(url="★이미지 url★")
-            await client.get_channel(int(channel)).send(embed=embed)
+            # 1명일시 'or message.author.id == ★유저id★' 삭제 / 이상 일시 'or message.author.id == ★유저id★'추가
+            if message.author.id == ★유저id★ or message.author.id == ★유저id★:
+                author = message.guild.get_member(int(message.author.id))
+                embed = discord.Embed(color=0x80E12A)
+                channel = ★전송될 채널 아이디★
+                embed.set_author(name=author, icon_url=message.author.avatar_url)
+                embed.add_field(name='관리자 출퇴근 알림', value='관리자가 출근하였습니다.')
+                embed.set_image(url="★이미지 url★")
+                await client.get_channel(int(channel)).send(embed=embed)
         except:
             pass
 
