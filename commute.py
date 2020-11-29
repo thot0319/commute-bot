0f07be82c7c7e3e05299be2573105ea2501be54a
DISCORDBBBB <60780527+BSGreatuser@users.noreply.github.com>
2020-11-29 12:25:45 +0900

    Update commute.py

--- commute.py
+++ commute.py
@@ -1,7 +1,7 @@
 import discord
 
 client = discord.Client()
-
+token = '★봇토큰★'
 
 @client.event
 async def on_ready():
@@ -9,19 +9,18 @@ async def on_ready():
     game = discord.Game('★표시될 게임이름★')
     await client.change_presence(status=discord.Status.online, activity=game)
 
-
+    
 @client.event
 async def on_message(message):
     if message.content.startswith("/출근"):
         try:
             # 메시지 관리 권한 있을시 사용가능
             if message.author.guild_permissions.manage_messages:
-                #author = message.guild.get_member(int(message.author.id))
                 embed = discord.Embed(color=0x80E12A)
                 channel = ★전송될 채널 아이디★
                 embed.set_author(name=message.author, icon_url=message.author.avatar_url)
                 embed.add_field(name='관리자 출퇴근 알림', value='관리자가 출근하였습니다.')
-                embed.set_image(url="★이미지 url★")
+                #embed.set_image(url="")
                 await client.get_channel(int(channel)).send(embed=embed)
         except:
             pass
@@ -29,14 +28,13 @@ async def on_message(message):
     if message.content.startswith("/퇴근"):
         try:
             if message.author.guild_permissions.manage_messages:
-                #author = message.guild.get_member(int(message.author.id))
                 embed = discord.Embed(color=0xFF0000)
                 channel = ★전송될 채널 아이디★
                 embed.set_author(name=message.author, icon_url=message.author.avatar_url)
                 embed.add_field(name='관리자 출퇴근 알림', value='관리자가 퇴근하였습니다.')
-                embed.set_image(url="★이미지 url★")
+                #embed.set_image(url="")
                 await client.get_channel(int(channel)).send(embed=embed)
         except:
             pass
 
-client.run('★봇 토큰★')
+client.run(token)
